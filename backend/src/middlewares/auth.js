f42c00249497aea21f817a55e7a6f1e0d825521c
Kevin Roan <kevinroan@proton.me>
2024-11-02 11:08:56 +0530

    fix; vulnerable packages

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,7 +1,7 @@
 const config = {
-  authRequired: false,
+  // authRequired: false,
   auth0Logout: true,
-  secret: process.env.AUTH_SECRET,
+  // secret: process.env.AUTH_SECRET,
   baseURL: "http://localhost:8000",
   clientID: "tlml29ifmAoAGLZz5v1inlMAP7oChSQK",
   issuerBaseURL: "https://dev-e7uxuudwsqqup47u.us.auth0.com",
