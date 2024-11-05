2a48a97402e06e8779e8c165f49fb52dff20f457
Kevin Roan <kevinroan@proton.me>
2024-11-05 13:27:32 +0530

    user verification

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -3,9 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("auth header", req.auth, req.query);
-  // check the email against users organization.participants doc
-  res.send("create user profile");
+  // dummy verification,
+  if (req.query.email === "kevinroan@gmail.com") {
+    console.log("user exists");
+    res.status(200).json({ status: "success", isExists: true });
+  } else {
+    console.log("user not found");
+    res.status(200).json({ status: "success", isExists: false });
+  }
 });
 
 module.exports = router;
