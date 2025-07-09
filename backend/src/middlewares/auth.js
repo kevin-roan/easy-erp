78e4dff38bb3fe7f23ca53148a681733ff78f2f7
Kevin Roan <kevinroan@proton.me>
2025-07-09 20:05:34 +0530

    chore; updated env with new auth creds

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -2,13 +2,10 @@ require("dotenv").config();
 
 const config = {
   auth0Logout: true,
-  secret:
-    process.env.SESSION_SECRET || "replace-with-a-very-long-random-string",
-  baseURL: process.env.BASE_URL || "http://localhost:8000",
-  clientID: process.env.AUTH0_CLIENT_ID || "uVRDwUfGkFiYGDRWot6ByieuLWNVhtId",
-  issuerBaseURL:
-    process.env.AUTH0_ISSUER_BASE_URL ||
-    "https://dev-e7uxuudwsqqup47u.us.auth0.com",
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 };
 
 module.exports = { config };
